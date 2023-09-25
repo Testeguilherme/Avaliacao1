a12fa823e27c23d1b6d90cb3fed16c2f4fe97498
Guilherme Frias <124912613+Testeguilherme@users.noreply.github.com>
2023-09-25 20:26:35 -0300

    Add files via upload

---
+++ Models/Medicamentos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIExemplo.Models
+{
+    public class Medicamentos
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set;}
+        [Required]
+        public string Lote { get; set; }
+        [Required]
+        public int MesVencimento { get; set; }
+        [Required]
+        public int AnoVencimento { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Fabricante { get; set; }
+    }
+}
